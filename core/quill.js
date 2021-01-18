f9893ab78c7b2d573383be72c72b98c9b3bbe762
user <amator015@gmail.com>
2021-01-18 17:40:11 +0200

    Refix pasteHTML Scrol Fix

--- core/quill.js
+++ core/quill.js
@@ -168,10 +168,10 @@ class Quill {
   }
 
   focus() {
-    // const { scrollTop } = this.scrollingContainer;
-    // this.selection.focus();
-    // this.scrollingContainer.scrollTop = scrollTop;
-    // this.scrollIntoView();
+    const { scrollTop } = this.scrollingContainer;
+    this.selection.focus();
+    this.scrollingContainer.scrollTop = scrollTop;
+    this.scrollIntoView();
   }
 
   format(name, value, source = Emitter.sources.API) {
@@ -374,7 +374,7 @@ class Quill {
   }
 
   scrollIntoView() {
-    // this.selection.scrollIntoView(this.scrollingContainer);
+    this.selection.scrollIntoView(this.scrollingContainer);
   }
 
   setContents(delta, source = Emitter.sources.API) {
